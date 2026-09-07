8ae026546bb132235e58a5fdaec9b0e01579734a
Roz Lagaban <166096424+SerendipitySunrise@users.noreply.github.com>
2026-09-08 00:35:55 +0800

    Refactor Student Information System in Dart

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 List<Map<String,dynamic>> Students = [];
-next_id = 1;
+
+int next_id = 1;
 
 void main(){
     while(true){
@@ -34,6 +35,9 @@ void main(){
             case 5: 
             print("Exiting Program");
             return;
+
+            default: 
+            print("Invalid Choice");
         }
     }
 
@@ -51,10 +55,14 @@ void addStudent(){
     String yearLevel = stdin.readLineSync()!;
 
     Students.add({
+        "ID": next_id,
         "Name": name,
         "Course": course,
         "Year Level": yearLevel
     });
+
+    next_id++;
+  
     print("Student Added Successfully ");
     
 }
@@ -64,30 +72,103 @@ void viewStudents(){
 
     if (Students.isEmpty){
         print("No records found");
+      return;
     }
 
     for (int i = 0; i < Students.length; i++){
         var student = Students[i];
 
+        print("ID: ${student["ID"]}");
         print("Name: ${student["Name"]}");
         print("Course: ${student["Course"]}");
-        print("Year Level: ${student["Year Level"]}\n");
-        print("=====================\n")
+        print("Year Level: ${student["Year Level"]}");
+        print("===================== \n");
 
     }
 }
 
+
+Map<String, dynamic>? searchStudent() {
+    
+    stdout.write("Enter student ID: ");
+    int id = int.parse(stdin.readLineSync()!);
+
+    for (int i = 0; i < Students.length; i++) {
+      if (Students[i]["ID"] == id) {
+        return Students[i];
+      }
+    }
+    return null;
+  }
+
 void updateStudent(){
     print("======Update Student Info======");
+  
+    if (Students.isEmpty) {
+      print("No records found.");
+      return;
+    }
+  
+    var student = searchStudent();
+  
+      if (student == null) {
+        print("Student not found");
+        return;
+      }
 
-    search student();
-}
+      print("Current Name: ${student["Name"]}");
+      print("Current Course: ${student["Course"]}");
+      print("Current Year Level: ${student["Year Level"]}\n");
+    
+      stdout.write("Enter new Name: ");
+      String name = stdin.readLineSync()!;
+    
+      stdout.write("Enter new Course: ");
+      String course = stdin.readLineSync()!;
+    
+      stdout.write("Enter new Year Level: ");
+      String yearLevel = stdin.readLineSync()!;
+    
+      student["Name"] = name;
+      student["Course"] = course;
+      student["Year Level"] = yearLevel;
+    
+      print("Student Updated Successfully!");
+    }
+
 
 void deleteStudent(){
     print("======Delete Student======");
+
+    if (Students.isEmpty) {
+      print("No records found.");
+      return;
+    }
+  
+    var student = searchStudent();
+  
+      if (student == null) {
+        print("Student not found");
+        return;
+      }
+
+      print("Student Found!");
+      print("ID: ${student["ID"]}");
+      print("Name: ${student["Name"]}\n");
+    
+      stdout.write("Are you sure you want to delete? (Y/N): ");
+      String answer = stdin.readLineSync()!;
+    
+      if (answer.toUpperCase() == "Y") {
+        Students.remove(student);
+    
+        print("Student Deleted Successfully!");
+      } else {
+        print("Delete Cancelled.");
+      }
 }
 
-void searchStudent
+
 
 //ASSIGNMENT
 //update
